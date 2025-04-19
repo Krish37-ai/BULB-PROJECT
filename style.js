f5040075423c8ca48db14d7e4c73ccfde2541ed0
Krish37-ai <krishbhai546@gmail.com>
2025-04-19 14:31:21 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+function on(){
+    document.getElementById("light").style.backgroundColor="cyan"
+    document.getElementById("light").style.boxShadow="0 0 20px cyan"
+}
+
+function off(){
+    document.getElementById("light").style.backgroundColor="white"
+    document.getElementById("light").style.boxShadow="1 1 20px white"
+}
